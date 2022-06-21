5b4b54bae63bf523cc119e5dff9b8ee6f7cf9237
Ryan Govender <tornaldo07.rg@gmail.com>
2022-06-21 21:33:57 +0200

    added extension for IConfig

--- TradingBot.Domain/Exchanges/Binance/Common/BinanceConnection.cs
+++ TradingBot.Domain/Exchanges/Binance/Common/BinanceConnection.cs
@@ -2,21 +2,29 @@
 using Binance.Net.Objects;
 using CryptoExchange.Net.Authentication;
 using CryptoExchange.Net.Objects;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TradingBot.Domain.Extensions;
 
 namespace TradingBot.Domain.Exchanges.Binance.Common
 {
     internal class BinanceConnection : IBinanceConnection
     {
+        private readonly IConfiguration _config;
+        public BinanceConnection(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
         public BinanceClient CreateBinanceClient()
         {
             var binanceClient = new BinanceClient(new BinanceClientOptions()
             {
-                ApiCredentials = new ApiCredentials("WqXgTPBUpALIVM3ULGGHhuLXNEsogJIhsnjmkpTuM2zyPJpwVRfqOaa6VZg654Vf", "oNB7xetILJl9iQxsi0hFMQCkNZVT98h0titLAdJXtmNwFWKdffP0M9rcTL7XPFxC"),
+                ApiCredentials = new ApiCredentials(_config.GetBinanceKey("API-KEY"),_config.GetBinanceKey("SECRET-KEY")),
                 // Set options here for this client
 
                 SpotApiOptions = new BinanceApiClientOptions
@@ -24,7 +32,7 @@ namespace TradingBot.Domain.Exchanges.Binance.Common
                     BaseAddress = "https://testnet.binance.vision",
                     RateLimitingBehaviour = RateLimitingBehaviour.Fail
                 },
-            });
+            }); ;
 
             return binanceClient;
         }
---
+++ TradingBot.Domain/Extensions/IConfigurationExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingBot.Domain.Extensions
+{
+    internal static class IConfigurationExtension
+    {
+        public static string GetBinanceKey(this IConfiguration configuration, string keyName)
+        {
+            return configuration.GetAppsetting($"Binance:{keyName}");
+        }
+
+        public static string GetAppsetting(this IConfiguration configuration, string sectionName)
+        {
+            var configValue = configuration.GetSection(sectionName);
+
+            if (configValue is null || !configValue.Exists()) throw new KeyNotFoundException();
+
+            return configValue.Value;
+        }
+    }
+}
